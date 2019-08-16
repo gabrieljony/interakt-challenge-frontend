9a5e5919068785e2b4790cbfb7e8425ba04ff816
Gabriel Jony <gabjony@gmail.com>
2019-08-15 23:09:42 -0300

    ajuste das rotas da aplicação

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import App from "./pages/App";
+import Home from "./pages/Home";
+import Delivery from "./pages/Delivery";
+import Products from "./pages/Products";
 import Error404 from "./pages/Error404";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-        <Route path="/" exact={true} component={App} />
+        <App>
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/delivery" exact={true} component={Delivery} />
+            <Route path="/products" exact={true} component={Products} />
+        </App>
         <Route path="*" exact={true} component={Error404} />
     </Switch>
   </BrowserRouter>,
