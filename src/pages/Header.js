77534ae7423cde0130bfb6f3f7bd38f4265f1531
Gabriel Jony <gabjony@gmail.com>
2019-08-15 23:19:38 -0300

    adicionado icon user no header

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react'
 
 import styled from "styled-components";
 
+import { Icon } from 'antd';
+
 export default class Header extends Component {
     render() {
         return (
             <Menu>
                 <h1>INTERAKT | Challenge</h1>
+                <Icon type="user" theme="outlined" />
             </Menu>
         )
     }
@@ -16,5 +19,8 @@ export const Menu = styled.header`
   background-color: #fafafa;
   color: #23314e;
   padding: 15px 30px;
+  font-size: 120%;
   box-shadow: 2px 5px 10px #627179;
+  display: flex;
+  justify-content: space-between
 `;
