ebef381b05a184b14fb13e1cb4b525fbda237266
Gabriel Jony <gabjony@gmail.com>
2019-08-24 20:53:20 -0300

    build de produção na pasta public

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
 	entry: path.resolve(__dirname, 'src', 'index.js'),
 
 	output: {
-		path: path.resolve(__dirname, 'build'),
+		path: path.resolve(__dirname, 'public'),
 		filename: '[name].[contentHash].bundle.js'
 	},
 
